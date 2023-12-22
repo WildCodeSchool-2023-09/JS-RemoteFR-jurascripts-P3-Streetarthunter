93e18cee01f14c7c10762bf2956f6af2881d53a6
Echeverria-Didier <didbidean@hotmail.com>
2023-12-22 10:49:21 +0100

    import Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import NavBar from "./components/NavBar";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      Hello
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
