c5e73b7978e43993a38de85d25c63b7cf37cc344
Alexandre-27 <alexandre24102000@gmail.com>
2024-01-07 23:42:21 +0100

    Created the /map route to access the map, removed the Map.jsx component in App.jsx. Adjusted map css.

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -10,12 +10,16 @@ function Map() {
   const mapRef = useRef();
 
   return (
-    <MapContainer center={[48.8566, 2.3522]} zoom={ZOOM_LEVEL} ref={mapRef}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </MapContainer>
+    <>
+      <h1>LOGO</h1>
+      <MapContainer center={[48.8566, 2.3522]} zoom={ZOOM_LEVEL} ref={mapRef}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </MapContainer>
+      <h1>NavBar</h1>
+    </>
   );
 }
 
