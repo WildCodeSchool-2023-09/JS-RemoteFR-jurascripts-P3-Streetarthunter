7c3c1cec6853633aaad2d1252eb59c9b97109906
Echeverria-Didier <didbidean@hotmail.com>
2024-01-08 18:06:34 +0100

    added a NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,25 @@
-function navBar() {
-  return <p>test</p>;
+import "./NavBar.scss";
+import "../styles/commons.scss";
+
+function NavBar() {
+  return (
+    <section>
+      <h1>STREET ART HUNTER</h1>
+      <nav className="navbar">
+        <h3>ACCEUIL</h3>
+        <h3>GALERIE</h3>
+        <h3>CARTE</h3>
+        <div>
+          <img src="./src/assets/picto/yellow/profile_yell.png" alt="" />
+          <p>Inscription</p>
+        </div>
+        <div>
+          <img src="./src/assets/picto/yellow/connexion-yell.png" alt="" />
+          <p>Connexion</p>
+        </div>
+      </nav>
+    </section>
+  );
 }
 
-export default navBar;
+export default NavBar;
