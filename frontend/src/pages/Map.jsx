ec34963621eae5c2a626ac335cc992d29e63fcc2
Alexandre-27 <alexandre24102000@gmail.com>
2024-01-09 17:56:40 +0100

    Structuring the map page with the implementation of css with display flex, border radius etc in the Map.scss file. Added div and class in the Map.jsx file to configure the scss and added the button in the media screen in the commons.scss file.

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -84,22 +84,52 @@ function Map() {
             </MapContainer>
           )}
           <h2>Ajouter une oeuvre</h2>
+          <p>Logo de Téléchargement</p>
           <p>Télécharger ma photo</p>
-          <section>
-            <p>Nom de l'artiste</p>
-            <input />
-            <p>Nom de l'oeuvre</p>
-            <input />
-            <p>Adresse</p>
-            <input />
-            <p>Ville</p>
-            <input />
-            <p>Descritpion de l'oeuvre</p>
-            <input />
-            <p>A propos de l'artiste (bonus)</p>
-            <input />
-            <button type="button">Soumettre</button>
-          </section>
+          <form className="content">
+            <div className="parent-div">
+              <div>
+                <div className="align-div">
+                  <p>
+                    Nom de l'artiste <span className="cyan-span">*</span>
+                  </p>
+                  <input />
+                </div>
+                <div className="align-div">
+                  <p>Nom de l'oeuvre</p>
+                  <input />
+                </div>
+                <div className="align-div">
+                  <p>
+                    Adresse <span className="cyan-span">*</span>
+                  </p>
+                  <input />
+                </div>
+                <div className="align-div">
+                  <p>
+                    Ville <span className="cyan-span">*</span>
+                  </p>
+                  <input />
+                </div>
+              </div>
+              <div>
+                <div className="align-div">
+                  <p>
+                    Descritpion de l'oeuvre <span className="cyan-span">*</span>
+                  </p>
+                  <input className="desc-inp" />
+                </div>
+                <div className="align-div">
+                  <p>
+                    A propos de l'artiste
+                    <span className="cyan-span"> (bonus)</span>
+                  </p>
+                  <input className="about-inp" />
+                </div>
+              </div>
+            </div>
+          </form>
+          <button type="button">Soumettre</button>
         </>
       )}
     </div>
