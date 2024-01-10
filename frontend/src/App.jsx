b3ddc46817a2b4f38d7bcf8611bc4aad0f01fab6
Echeverria-Didier <didbidean@hotmail.com>
2024-01-10 17:30:53 +0100

    création NavBar mobile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,48 @@
+import { useMediaQuery } from "@react-hook/media-query";
+
 import NavBar from "./components/NavBar";
+import NavBarM from "./components/NavBarM";
 import "./styles/commons.scss";
 
 function App() {
+  const isMobile = useMediaQuery("only screen and (max-width: 600px)");
+
   return (
-    <>
-      <NavBar />
-      <header className="App-header">
-        <h2>Le site des chasseurs d'art urbain !</h2>
-        <h3>En cours de développement...</h3>
-        <p>
-          Passionnés de street-art, patientez encore quelques semaines pour
-          jouer au seul jeu qui vous permettra de combiner art, compétition et
-          orientation !
-        </p>
-        <br />
-        <p className="small-text">Un site CreaScript</p>
-        <br />
-      </header>
-    </>
+    <div>
+      {isMobile ? (
+        <>
+          <header className="App-header">
+            <h2>Le site des chasseurs d'art urbain !</h2>
+            <h3>En cours de développement...</h3>
+            <p>
+              Passionnés de street-art, patientez encore quelques semaines pour
+              jouer au seul jeu qui vous permettra de combiner art, compétition
+              et orientation !
+            </p>
+            <br />
+            <p className="small-text">Un site CreaScript</p>
+            <br />
+          </header>
+          <NavBarM />
+        </>
+      ) : (
+        <div>
+          <NavBar />
+          <header className="App-header">
+            <h2>Le site des chasseurs d'art urbain !</h2>
+            <h3>En cours de développement...</h3>
+            <p>
+              Passionnés de street-art, patientez encore quelques semaines pour
+              jouer au seul jeu qui vous permettra de combiner art, compétition
+              et orientation !
+            </p>
+            <br />
+            <p className="small-text">Un site CreaScript</p>
+            <br />
+          </header>
+        </div>
+      )}
+    </div>
   );
 }
 
