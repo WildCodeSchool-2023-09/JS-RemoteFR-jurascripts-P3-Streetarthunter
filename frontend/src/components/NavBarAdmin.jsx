7435e1a14b5f5797e7b6492af0939ccc09e24a21
Alexandre-27 <alexandre24102000@gmail.com>
2024-01-12 11:17:58 +0100

    Setting up tags in the NavBarAdmin.jsx component

--- frontend/src/components/NavBarAdmin.jsx
+++ frontend/src/components/NavBarAdmin.jsx
@@ -1,5 +1,12 @@
 function NavBarAdmin() {
-  return <h1>Hello</h1>;
+  return (
+    <nav>
+      <h3>Tableau de bord</h3>
+      <h3>Utilisateurs</h3>
+      <h3>Street Art Hunter</h3>
+      <h3>Artistes</h3>
+    </nav>
+  );
 }
 
 export default NavBarAdmin;
