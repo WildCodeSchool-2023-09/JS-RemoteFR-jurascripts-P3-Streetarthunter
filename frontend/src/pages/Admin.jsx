f91dc8eb74bdefd3864d3c2b4803c519255d92ee
Alexandre-27 <alexandre24102000@gmail.com>
2024-01-13 13:14:53 +0100

    Implementation of a hover and an active which applies from section to section via a window.scrollY on the NavBarAdmin a tags. ! Problems ! --> the active tag a of the artists section does not apply and for the rest of the sections it applies after the title of the section h2 passed. The cause is that the Top offset only applies when the section reaches the top of the screen and not when the NavBarAdmin arrives.

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "@react-hook/media-query";
 import "./Admin.scss";
 import NavBar from "../components/NavBar";
@@ -13,201 +14,245 @@ import download from "../assets/panel-admin/download-svgrepo-com.svg";
 
 function admin() {
   const isMobile = useMediaQuery("only screen and (min-width: 600px)");
+  const [activeSection, setActiveSection] = useState("dashboard");
+  const dashboardRef = useRef(null);
+  const usersRef = useRef(null);
+  const streetArtRef = useRef(null);
+  const artistsRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+
+      if (
+        offset >= dashboardRef.current.offsetTop &&
+        offset < usersRef.current.offsetTop
+      ) {
+        setActiveSection("dashboard");
+      } else if (
+        offset >= usersRef.current.offsetTop &&
+        offset < streetArtRef.current.offsetTop
+      ) {
+        setActiveSection("users");
+      } else if (
+        offset >= streetArtRef.current.offsetTop &&
+        offset < artistsRef.current.offsetTop
+      ) {
+        setActiveSection("streetArt");
+      } else if (offset >= artistsRef.current.offsetTop) {
+        setActiveSection("artists");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <>
       <NavBar />
       {isMobile ? (
         <>
-          <NavBarAdmin />
-          <div className="notif-h2-img" id="notif">
-            <h2 className="border-none-h2">Notifications</h2>
-            <img alt="ampoule" src={light} />
-          </div>
-          <div className="notif-parent-div">
-            <div className="notif-grid-div notif-child-div-yellow">
-              <p>12 Street Art en attente de validation</p>
-              <img alt="Sablier" src={Hourglass} />
+          <NavBarAdmin activeSection={activeSection} />
+          <section ref={dashboardRef}>
+            <div className="notif-h2-img" id="dashboard">
+              <h2 className="border-none-h2">Notifications</h2>
+              <img alt="ampoule" src={light} />
             </div>
-            <div className="notif-grid-div notif-child-div-cyan">
-              <p>3 Street Art sont portés disparus</p>
-              <img alt="Carte" src={MapAndFlag} />
+            <div className="notif-parent-div">
+              <div className="notif-grid-div notif-child-div-yellow">
+                <p>12 Street Art en attente de validation</p>
+                <img alt="Sablier" src={Hourglass} />
+              </div>
+              <div className="notif-grid-div notif-child-div-cyan">
+                <p>3 Street Art sont portés disparus</p>
+                <img alt="Carte" src={MapAndFlag} />
+              </div>
             </div>
-          </div>
-          <h2 className="admin-h2" id="uti">
-            Utilisateurs
-          </h2>
-          <div className="uti-flex">
-            <div className="uti-grid">
-              <button type="button" className="uti-filter-button">
-                Filtrer
+          </section>
+          <section ref={usersRef}>
+            <h2 className="admin-h2" id="users">
+              Utilisateurs
+            </h2>
+            <div className="uti-flex">
+              <div className="uti-grid">
+                <button type="button" className="uti-filter-button">
+                  Filtrer
+                </button>
+                <input
+                  className="uti-research-input"
+                  type="text"
+                  placeholder="Recherchez..."
+                />
+              </div>
+            </div>
+            <div className="profil-uti-parent">
+              <div className="profil-uti-child">
+                <img
+                  alt="avatar du profil"
+                  className="avatar-grid"
+                  src={Avatar}
+                />
+                <img alt="badge du profil" className="badge-grid" src={badge} />
+                <p className="pseudo-grid">Pseudo</p>
+                <p className="name-grid">Prénom Nom</p>
+                <p className="capture-grid">
+                  Capture <span className="capture-grid-red">12</span>
+                </p>
+                <p className="ranking-grid">
+                  Classement <span className="ranking-grid-red">203</span>
+                </p>
+                <p className="points-grid">
+                  Points <span className="points-grid-red">1255</span>
+                </p>
+              </div>
+              <div className="profil-uti-child">
+                <img
+                  alt="avatar du profil"
+                  className="avatar-grid"
+                  src={Avatar}
+                />
+                <img alt="badge du profil" className="badge-grid" src={badge} />
+                <p className="pseudo-grid">Pseudo</p>
+                <p className="name-grid">Prénom Nom</p>
+                <p className="capture-grid">
+                  Capture <span className="capture-grid-red">12</span>
+                </p>
+                <p className="ranking-grid">
+                  Classement <span className="ranking-grid-red">203</span>
+                </p>
+                <p className="points-grid">
+                  Poitns <span className="points-grid-red">1255</span>
+                </p>
+              </div>
+              <div className="profil-uti-child">
+                <img
+                  alt="avatar du profil"
+                  className="avatar-grid"
+                  src={Avatar}
+                />
+                <img alt="badge du profil" className="badge-grid" src={badge} />
+                <p className="pseudo-grid">Pseudo</p>
+                <p className="name-grid">Prénom Nom</p>
+                <p className="capture-grid">
+                  Capture <span className="capture-grid-red">12</span>
+                </p>
+                <p className="ranking-grid">
+                  Classement <span className="ranking-grid-red">203</span>
+                </p>
+                <p className="points-grid">
+                  Poitns <span className="points-grid-red">1255</span>
+                </p>
+              </div>
+              <div className="profil-uti-child">
+                <img
+                  alt="avatar du profil"
+                  className="avatar-grid"
+                  src={Avatar}
+                />
+                <img alt="badge du profil" className="badge-grid" src={badge} />
+                <p className="pseudo-grid">Pseudo</p>
+                <p className="name-grid">Prénom Nom</p>
+                <p className="capture-grid">
+                  Capture <span className="capture-grid-red">12</span>
+                </p>
+                <p className="ranking-grid">
+                  Classement <span className="ranking-grid-red">203</span>
+                </p>
+                <p className="points-grid">
+                  Poitns <span className="points-grid-red">1255</span>
+                </p>
+              </div>
+              <div className="profil-uti-child">
+                <img
+                  alt="avatar du profil"
+                  className="avatar-grid"
+                  src={Avatar}
+                />
+                <img alt="badge du profil" className="badge-grid" src={badge} />
+                <p className="pseudo-grid">Pseudo</p>
+                <p className="name-grid">Prénom Nom</p>
+                <p className="capture-grid">
+                  Capture <span className="capture-grid-red">12</span>
+                </p>
+                <p className="ranking-grid">
+                  Classement <span className="ranking-grid-red">203</span>
+                </p>
+                <p className="points-grid">
+                  Poitns <span className="points-grid-red">1255</span>
+                </p>
+              </div>
+              <div className="profil-uti-child">
+                <img
+                  alt="avatar du profil"
+                  className="avatar-grid"
+                  src={Avatar}
+                />
+                <img alt="badge du profil" className="badge-grid" src={badge} />
+                <p className="pseudo-grid">Pseudo</p>
+                <p className="name-grid">Prénom Nom</p>
+                <p className="capture-grid">
+                  Capture <span className="capture-grid-red">12</span>
+                </p>
+                <p className="ranking-grid">
+                  Classement <span className="ranking-grid-red">203</span>
+                </p>
+                <p className="points-grid">
+                  Poitns <span className="points-grid-red">1255</span>
+                </p>
+              </div>
+            </div>
+          </section>
+          <section ref={streetArtRef}>
+            <h2 className="admin-h2" id="streetArt">
+              Street Art
+            </h2>
+            <NavBarSa />
+            <div className="sa-grid">
+              <img alt="Street Art" className="img-grid" src={strafari} />
+              <p className="posted-grid">
+                Posté par{" "}
+                <span className="posted-grid-red">Pseudo de l'utilisateur</span>
+              </p>
+              <button type="button" className="validate-grid">
+                Valider
               </button>
-              <input
-                className="uti-research-input"
-                type="text"
-                placeholder="Recherchez..."
-              />
-            </div>
-          </div>
-          <div className="profil-uti-parent">
-            <div className="profil-uti-child">
+              <button type="button" className="refuse-grid">
+                Refuser
+              </button>
               <img
-                alt="avatar du profil"
-                className="avatar-grid"
-                src={Avatar}
+                alt="Street Art"
+                className="original-img-grid"
+                src={strafari}
               />
-              <img alt="badge du profil" className="badge-grid" src={badge} />
-              <p className="pseudo-grid">Pseudo</p>
-              <p className="name-grid">Prénom Nom</p>
-              <p className="capture-grid">
-                Capture <span className="capture-grid-red">12</span>
-              </p>
-              <p className="ranking-grid">
-                Classement <span className="ranking-grid-red">203</span>
-              </p>
-              <p className="points-grid">
-                Points <span className="points-grid-red">1255</span>
-              </p>
+              <p className="original-work-grid">Oeuvre Originale</p>
             </div>
-            <div className="profil-uti-child">
-              <img
-                alt="avatar du profil"
-                className="avatar-grid"
-                src={Avatar}
-              />
-              <img alt="badge du profil" className="badge-grid" src={badge} />
-              <p className="pseudo-grid">Pseudo</p>
-              <p className="name-grid">Prénom Nom</p>
-              <p className="capture-grid">
-                Capture <span className="capture-grid-red">12</span>
-              </p>
-              <p className="ranking-grid">
-                Classement <span className="ranking-grid-red">203</span>
-              </p>
-              <p className="points-grid">
-                Poitns <span className="points-grid-red">1255</span>
-              </p>
-            </div>
-            <div className="profil-uti-child">
-              <img
-                alt="avatar du profil"
-                className="avatar-grid"
-                src={Avatar}
-              />
-              <img alt="badge du profil" className="badge-grid" src={badge} />
-              <p className="pseudo-grid">Pseudo</p>
-              <p className="name-grid">Prénom Nom</p>
-              <p className="capture-grid">
-                Capture <span className="capture-grid-red">12</span>
-              </p>
-              <p className="ranking-grid">
-                Classement <span className="ranking-grid-red">203</span>
-              </p>
-              <p className="points-grid">
-                Poitns <span className="points-grid-red">1255</span>
-              </p>
-            </div>
-            <div className="profil-uti-child">
-              <img
-                alt="avatar du profil"
-                className="avatar-grid"
-                src={Avatar}
-              />
-              <img alt="badge du profil" className="badge-grid" src={badge} />
-              <p className="pseudo-grid">Pseudo</p>
-              <p className="name-grid">Prénom Nom</p>
-              <p className="capture-grid">
-                Capture <span className="capture-grid-red">12</span>
-              </p>
-              <p className="ranking-grid">
-                Classement <span className="ranking-grid-red">203</span>
-              </p>
-              <p className="points-grid">
-                Poitns <span className="points-grid-red">1255</span>
-              </p>
-            </div>
-            <div className="profil-uti-child">
-              <img
-                alt="avatar du profil"
-                className="avatar-grid"
-                src={Avatar}
-              />
-              <img alt="badge du profil" className="badge-grid" src={badge} />
-              <p className="pseudo-grid">Pseudo</p>
-              <p className="name-grid">Prénom Nom</p>
-              <p className="capture-grid">
-                Capture <span className="capture-grid-red">12</span>
-              </p>
-              <p className="ranking-grid">
-                Classement <span className="ranking-grid-red">203</span>
-              </p>
-              <p className="points-grid">
-                Poitns <span className="points-grid-red">1255</span>
-              </p>
-            </div>
-            <div className="profil-uti-child">
-              <img
-                alt="avatar du profil"
-                className="avatar-grid"
-                src={Avatar}
-              />
-              <img alt="badge du profil" className="badge-grid" src={badge} />
-              <p className="pseudo-grid">Pseudo</p>
-              <p className="name-grid">Prénom Nom</p>
-              <p className="capture-grid">
-                Capture <span className="capture-grid-red">12</span>
-              </p>
-              <p className="ranking-grid">
-                Classement <span className="ranking-grid-red">203</span>
-              </p>
-              <p className="points-grid">
-                Poitns <span className="points-grid-red">1255</span>
-              </p>
-            </div>
-          </div>
-          <h2 className="admin-h2" id="sa">
-            Street Art
-          </h2>
-          <NavBarSa />
-          <div className="sa-grid">
-            <img alt="Street Art" className="img-grid" src={strafari} />
-            <p className="posted-grid">
-              Posté par{" "}
-              <span className="posted-grid-red">Pseudo de l'utilisateur</span>
-            </p>
-            <button type="button" className="validate-grid">
-              Valider
+          </section>
+          <section ref={artistsRef}>
+            <h2 className="admin-h2" id="artists">
+              Ajouter un artiste
+            </h2>
+            <img src={download} alt="download" className="download-art-img" />
+            <p>Télécharger une photo</p>
+            <form className="add-art-form-grid">
+              <div className="name-art-grid">
+                <p>
+                  Nom de l'artiste <span className="name-red-span">*</span>
+                </p>
+                <input />
+              </div>
+              <div className="about-art-grid">
+                <p>A propos de l'artiste</p>
+                <input className="about-art-pad" />
+              </div>
+            </form>
+            <button type="button" className="add-art-button">
+              Ajouter
             </button>
-            <button type="button" className="refuse-grid">
-              Refuser
-            </button>
-            <img
-              alt="Street Art"
-              className="original-img-grid"
-              src={strafari}
-            />
-            <p className="original-work-grid">Oeuvre Originale</p>
-          </div>
-          <h2 className="admin-h2" id="art">
-            Ajouter un artiste
-          </h2>
-          <img src={download} alt="download" className="download-art-img" />
-          <p>Télécharger une photo</p>
-          <form className="add-art-form-grid">
-            <div className="name-art-grid">
-              <p>
-                Nom de l'artiste <span className="name-red-span">*</span>
-              </p>
-              <input />
-            </div>
-            <div className="about-art-grid">
-              <p>A propos de l'artiste</p>
-              <input className="about-art-pad" />
-            </div>
-          </form>
-          <button type="button" className="add-art-button">
-            Ajouter
-          </button>
+          </section>
         </>
       ) : (
         <h3 className="admin-mobile-h2">
