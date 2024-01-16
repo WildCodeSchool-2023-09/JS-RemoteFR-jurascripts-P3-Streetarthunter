996f4a91ff90c92ad8fad4c3594d8cac3207924d
Alexandre-27 <alexandre24102000@gmail.com>
2024-01-16 11:17:15 +0100

    Creation of an activePage state which is equivalent to home and creation of an anonymous function handleChangePage which has a page parameter and which contains setActivePage inside

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { useMediaQuery } from "@react-hook/media-query";
 import NavBar from "../components/NavBar";
@@ -6,6 +7,11 @@ import logo from "../assets/Logo.svg";
 
 function Layout() {
   const isMobile = useMediaQuery("only screen and (max-width: 600px)");
+  const [activePage, setActivePage] = useState("accueil");
+
+  const handleChangePage = (page) => {
+    setActivePage(page);
+  };
 
   return (
     <div>
@@ -19,7 +25,7 @@ function Layout() {
         </>
       ) : (
         <>
-          <NavBar />
+          <NavBar activePage={activePage} handleChangePage={handleChangePage} />
           <Outlet />
         </>
       )}
