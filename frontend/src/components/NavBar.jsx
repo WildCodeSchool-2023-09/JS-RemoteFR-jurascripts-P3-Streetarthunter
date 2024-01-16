24ae3f295cd31f4c4bb5d043a35e49ea86a47aa5
Alexandre-27 <alexandre24102000@gmail.com>
2024-01-16 11:24:52 +0100

    Uses props coming from the Layout.jsx file by putting a ternary which checks that the activePage state corresponds to the requested value and if it corresponds a className is also applied to the Link. Using the handleChangePage in an Onclick that returns a value to the setActivePage to change the value in the activePage sate.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,22 +1,40 @@
 import { Link } from "react-router-dom";
-
+import PropTypes from "prop-types";
 import "./NavBar.scss";
 import "../styles/commons.scss";
 import login from "../assets/picto/white/connexion_white.svg";
 import register from "../assets/picto/white/profil_white.svg";
 
-function NavBar() {
+function NavBar({ activePage, handleChangePage }) {
   return (
     <section className="nav-container">
       <h1>STREET ART HUNTER</h1>
       <nav className="navbar">
-        <Link to="/" className="img-nav-link">
+        <Link
+          to="/"
+          className={`img-nav-link ${activePage === "accueil" ? "active" : ""}`}
+          onClick={() => {
+            handleChangePage("accueil");
+          }}
+        >
           <h3>ACCUEIL</h3>
         </Link>
-        <Link to="/galerie" className="img-nav-link">
+        <Link
+          to="/galerie"
+          className={`img-nav-link ${activePage === "galerie" ? "active" : ""}`}
+          onClick={() => {
+            handleChangePage("galerie");
+          }}
+        >
           <h3>GALERIE</h3>
         </Link>
-        <Link to="/carte" className="img-nav-link">
+        <Link
+          to="/carte"
+          className={`img-nav-link ${activePage === "carte" ? "active" : ""}`}
+          onClick={() => {
+            handleChangePage("carte");
+          }}
+        >
           <h3>CARTE</h3>
         </Link>
         <Link to="/inscription" className="img-nav-link">
@@ -31,5 +49,11 @@ function NavBar() {
     </section>
   );
 }
+
+NavBar.propTypes = {
+  activePage: PropTypes.oneOf(["accueil", "galerie", "carte", "admin"])
+    .isRequired,
+  handleChangePage: PropTypes.func.isRequired,
+};
 
 export default NavBar;
