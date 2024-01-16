86d0c0835f9fda7ad39079df9114c17567d8848a
Alexandre-27 <alexandre24102000@gmail.com>
2024-01-16 10:42:02 +0100

    Addition of the NavBarM.jsx component in the Layout.jsx page and an isMobile ternary in the Layout.jsx page which checks if the screen is less or more than 600px and which adapts between the NavBar.jsx component and NavBarM.jsx depending on the ternary response.

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,12 +1,25 @@
 import { Outlet } from "react-router-dom";
+import { useMediaQuery } from "@react-hook/media-query";
 import NavBar from "../components/NavBar";
+import NavBarM from "../components/NavBarM";
 
 function Layout() {
+  const isMobile = useMediaQuery("only screen and (max-width: 600px)");
+
   return (
-    <>
-      <NavBar />
-      <Outlet />
-    </>
+    <div>
+      {isMobile ? (
+        <>
+          <Outlet />
+          <NavBarM />
+        </>
+      ) : (
+        <>
+          <NavBar />
+          <Outlet />
+        </>
+      )}
+    </div>
   );
 }
 
