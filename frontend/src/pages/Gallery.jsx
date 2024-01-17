0020ec5ebc7c584cdfe198f32ab9271b4330bdb8
Laynaria <pro.laynaria@outlook.fr>
2024-01-17 17:07:47 +0100

    fetching pictures and border color fix

--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./Gallery.scss";
 
 function Gallery() {
+  const [artworks, setArtworks] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/api/artworks`)
+      .then((res) => setArtworks(res.data))
+      .catch((err) =>
+        console.error("Erreur lors de la récupération des données :", err)
+      );
+  }, []);
+
   return (
     <section className="gallery">
       <h1>Gallery</h1>
+
+      {artworks
+        ?.filter((artwork, index) => index < 10)
+        .map((artwork) => (
+          <img src={artwork.picture} key={artwork.id} alt={artwork.title} />
+        ))}
     </section>
   );
 }
