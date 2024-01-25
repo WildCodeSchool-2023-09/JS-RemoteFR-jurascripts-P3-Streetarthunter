4eb25079cd5af535b7a56ee0e5d337114c3f5f78
Suzy-cas <contact@scassar.com>
2024-01-25 14:53:48 +0100

    fixed redirecton

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,5 @@
 import { useState, useContext } from "react";
+import { jwtDecode } from "jwt-decode";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
@@ -8,19 +9,33 @@ import "../styles/modals.scss";
 import CrossButton from "../assets/picto/yellow/cross_yell.svg";
 
 function Login() {
-  const { user, handleAuth } = useContext(AuthContext);
+  const { handleAuth } = useContext(AuthContext);
   const [loginInfo, setLoginInfo] = useState({
     email: "",
     password: "",
   });
 
   const navigate = useNavigate();
-  const redirectTo = () => {
-    if (!user.is_administrator) {
-      return navigate("/admin");
-    }
 
-    return navigate("/profil");
+  const redirectTo = async () => {
+    const getToken = localStorage.getItem("token");
+    if (getToken) {
+      const decodeToken = jwtDecode(getToken);
+      const userId = decodeToken.user_id;
+
+      try {
+        const { data } = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/api/users/${userId}`
+        );
+
+        if (data.is_administrator === 1) {
+          return navigate("/user/admin");
+        }
+      } catch (error) {
+        console.warn("Une erreur est survenue!", error);
+      }
+    }
+    return navigate("/carte");
   };
 
   const handleLoginRegister = (event) => {
@@ -28,7 +43,7 @@ function Login() {
     setLoginInfo({ ...loginInfo, [name]: value });
   };
 
-  // créer la fonction handleLogin avec ses filtres pour vérifier que tous les champs sont remplis et que l'adresse mail n'existe pas déjà + récupérer le JWT token
+  // créer la fonction handleLogin avec ses filtres pour vérifier que tous les champs sont remplis et que l'adresse mail n'existe pas déjà
   const handleLogin = async (e) => {
     e.preventDefault();
     const { email, password } = loginInfo;
@@ -43,10 +58,12 @@ function Login() {
         `${import.meta.env.VITE_BACKEND_URL}/api/login`,
         loginInfo
       );
+      // Récupération du token
       await localStorage.setItem("token", res.data.token);
+      // Décodage du token et récupération des données
       await handleAuth();
-      await redirectTo();
-      console.info(user);
+      // Redirection en fonction du statut de l'utilisateur
+      redirectTo();
     } catch (err) {
       console.error(err);
     }
