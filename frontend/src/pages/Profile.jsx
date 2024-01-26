f2599fbeb6c7eb3292efb6bbd294a60bb2d0087a
Echeverria-Didier <didbidean@hotmail.com>
2024-01-26 14:32:02 +0100

    finalisation de profile

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -12,28 +12,29 @@ function Profile() {
 
   return (
     <div className="profile-container">
-      <h1>Mon Profil</h1>
+      <h3>Mon Profil</h3>
 
-      <div className="avatar-container">
-        <img src={user.avatar} alt="user avatar" />
-        <p className="user-score">Score: {user.score}</p>
-      </div>
+      <div className="card-content">
+        <div className="avatar-container">
+          <img src={user.avatar} alt="user avatar" />
+          <p className="user-score">Score: {user.score}</p>
+        </div>
+        <div className="user-details">
+          <p className="firstname">{user.pseudo}</p>
+          <p>{user.username}</p>
+          <p>
+            {user.firstname} {user.lastname}
+          </p>
+          <p>Classement: {user.ranking}</p>
+        </div>
 
-      <div className="user-details">
-        <p>{user.pseudo}</p>
-        <p>{user.username}</p>
-        <p>
-          {user.firstname} {user.lastname}
-        </p>
-        <p>Classement: {user.ranking}</p>
-      </div>
-
-      <div className="lorem-ipsum">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia
-          voluptate nobis earum ratione soluta laboriosam similique facere amet
-          fuga corrupti!
-        </p>
+        <div className="bio">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia
+            voluptate nobis earum ratione soluta laboriosam similique facere
+            amet fuga corrupti!
+          </p>
+        </div>
       </div>
     </div>
   );
