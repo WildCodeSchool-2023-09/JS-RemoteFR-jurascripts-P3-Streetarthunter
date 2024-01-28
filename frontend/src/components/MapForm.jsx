a904b86e9049470860d3dde95e22f7f462cf4009
Alexis-NM <alexis.na.me@gmail.com>
2024-01-28 12:35:10 +0100

    insert fake data

--- frontend/src/components/MapForm.jsx
+++ frontend/src/components/MapForm.jsx
@@ -1,82 +1,65 @@
 import { useState } from "react";
 import axios from "axios";
-import { v4 as uuidv4 } from "uuid"; // Importez la fonction v4 de uuid
 import Dropzone from "./DropZone";
 import "./MapForm.scss";
 
 function MapForm() {
   const [userLogged, setUserLogged] = useState(false);
-  const [formData, setFormData] = useState({
-    id: uuidv4(), // Générez un nouvel ID automatiquement
-    artistName: "",
-    title: "",
-    street: "",
-    city: "",
-    description: "",
-    artistBio: "",
-  });
 
   const handleClick = () => {
     setUserLogged(true);
   };
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const validateJson = (data) => {
-    const requiredFields = [
-      "id",
-      "artistName",
-      "title",
-      "street",
-      "city",
-      "description",
-      "artistBio",
-    ];
-
-    for (const field of requiredFields) {
-      if (!(field in data)) {
-        console.error(`Le champ ${field} est manquant dans le fichier JSON.`);
-        return false;
-      }
-    }
-
-    return true;
-  };
-
   const handleSubmit = async () => {
     try {
-      if (!validateJson(formData)) {
-        console.error(
-          "Le formulaire ne correspond pas au format JSON attendu."
-        );
-        return;
-      }
+      const backendUrl =
+        import.meta.env.VITE_BACKEND_URL || "http://localhost:3310";
 
-      const response = await axios.post(
-        `${process.env.VITE_BACKEND_URL}/api/artworks`,
-        formData,
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
+      const locationData = {
+        city: "Paris",
+        country: "France",
+        post_code: 75013,
+        street: "Rue Gérard",
+        street_number: 62,
+        latitude: 48.8293,
+        longitude: 2.352,
+      };
+
+      const locationResponse = await axios.post(
+        `${backendUrl}/api/locations`,
+        locationData
       );
 
-      if (response.status === 200) {
-        // Traitement en cas de succès, par exemple, rediriger l'utilisateur ou effectuer d'autres actions
-        console.info("Formulaire soumis avec succès !");
-      } else {
-        // Traitement en cas d'erreur, par exemple, afficher un message d'erreur à l'utilisateur
-        console.error("Erreur lors de la soumission du formulaire");
+      if (!locationResponse.data) {
+        console.error(
+          "Erreur lors de la création de l'emplacement :",
+          locationResponse.data
+        );
+      }
+
+      const artworkData = {
+        title: "Vivre c'est de la bombe",
+        picture: "https://shorturl.at/civKS",
+        description:
+          "Une nouvelle collaboration entre Jace et Miss.Tic sur un mur de la Butte aux Cailles",
+        artist_id: 42,
+        user_id: 52,
+        general_gallery: false,
+        reported: false,
+        location_id: 159,
+      };
+
+      const artworkResponse = await axios.post(
+        `${backendUrl}/api/artworks`,
+        artworkData
+      );
+
+      if (!artworkResponse.data) {
+        throw new Error("Erreur lors de la création de l'œuvre d'art");
       }
     } catch (error) {
-      console.error("Erreur lors de la soumission du formulaire", error);
+      // Gestion des erreurs
+      console.error("Erreur lors de la soumission des données.", error.message);
     }
   };
 
@@ -84,7 +67,7 @@ function MapForm() {
     <div className="form-container">
       {userLogged ? (
         <>
-          <h2 className="add-h2">Ajouter une oeuvre</h2>
+          <h2 className="add-h2">Ajouter une œuvre</h2>
           <Dropzone />
           <form className="content">
             <div className="parent-div">
@@ -93,46 +76,38 @@ function MapForm() {
                   <p>
                     Nom de l'artiste <span className="cyan-span">*</span>
                   </p>
-                  <input name="artistName" onChange={handleInputChange} />
+                  <input name="artistName" />
                 </div>
                 <div className="align-div">
-                  <p>Nom de l'oeuvre</p>
-                  <input name="title" onChange={handleInputChange} />
+                  <p>Nom de l'œuvre</p>
+                  <input name="title" />
                 </div>
                 <div className="align-div">
                   <p>
                     Adresse <span className="cyan-span">*</span>
                   </p>
-                  <input name="street" onChange={handleInputChange} />
+                  <input name="street" />
                 </div>
                 <div className="align-div">
                   <p>
                     Ville <span className="cyan-span">*</span>
                   </p>
-                  <input name="city" onChange={handleInputChange} />
+                  <input name="city" />
                 </div>
               </div>
               <div className="right-container">
                 <div className="align-div">
                   <p>
-                    Descritpion de l'oeuvre <span className="cyan-span">*</span>
+                    Description de l'œuvre <span className="cyan-span">*</span>
                   </p>
-                  <input
-                    name="description"
-                    className="desc-inp"
-                    onChange={handleInputChange}
-                  />
+                  <input name="description" className="desc-inp" />
                 </div>
                 <div className="align-div">
                   <p>
                     À propos de l'artiste
                     <span className="cyan-span"> (bonus)</span>
                   </p>
-                  <input
-                    name="artistBio"
-                    className="about-inp"
-                    onChange={handleInputChange}
-                  />
+                  <input name="artistBio" className="about-inp" />
                 </div>
               </div>
             </div>
