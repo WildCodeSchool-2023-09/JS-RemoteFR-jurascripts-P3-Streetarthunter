d3cf29c8227c2637fb6ae90c2ac70417b009cf7d
Alexandre-27 <alexandre24102000@gmail.com>
2024-01-30 11:30:58 +0100

    Added NewArtAdmin.scss file in the ReportedArtAdmin.jsx component to apply the css and added calssName and tags in this same component.

--- frontend/src/components/ReportedArtAdmin.jsx
+++ frontend/src/components/ReportedArtAdmin.jsx
@@ -22,30 +22,52 @@ function ReportedArtAdmin() {
       });
   }, []);
 
+  const getFirstSentence = (text) => {
+    if (text === null || text === undefined) {
+      return "";
+    }
+
+    const sentences = text.split(".");
+    const firstSentence = sentences[0].trim();
+    return firstSentence;
+  };
+
   return (
     <>
       {artReported.map((artwork) => (
-        <div className="sa-grid">
-          <img alt="Street Art" className="img-grid" src={artwork.picture} />
-          <p className="posted-grid">
-            Posté par{" "}
-            <span className="posted-grid-red">{artwork.user_pseudo}</span>
-          </p>
-          <button type="button" className="validate-grid">
-            Annuler
-          </button>
-          <button type="button">Modifier</button>
-          <button type="button" className="refuse-grid">
-            Supprimer
-          </button>
-          <h3>Nom de l'oeuvre</h3>
-          <p>{artwork.title}</p>
-          <h3>Artiste</h3>
-          <p>{artwork.artist_name}</p>
-          <h3>Description</h3>
-          <p>{artwork.description}</p>
-          <h3>A propos de l'artiste</h3>
-          <p>{artwork.artist_bio}</p>
+        <div className="sa-flex-newart">
+          <div className="sa-element-left">
+            <img alt="Street Art" className="img-grid" src={artwork.picture} />
+            <p className="posted-grid">
+              Posté par{" "}
+              <span className="posted-grid-red">{artwork.user_pseudo}</span>
+            </p>
+          </div>
+          <div className="sa-element-right">
+            <h3 className="grid-namework-h3">Nom de l'oeuvre</h3>
+            <p className="grid-titleart-p">{artwork.title}</p>
+            <h3 className="grid-artist-h3">Artiste</h3>
+            <p className="grid-nameart-p">
+              {getFirstSentence(artwork.artist_name)}
+            </p>
+            <h3 className="grid-desc-h3">Description</h3>
+            <p className="grid-desc-h3">
+              {getFirstSentence(artwork.description)}
+            </p>
+            <h3 className="grid-aboutart-h3">A propos de l'artiste</h3>
+            <p className="grid-bioart-p">{artwork.artist_bio}</p>
+            <div className="sa-btn-div">
+              <button type="button" className="validate-grid">
+                Valider
+              </button>
+              <button type="button" className="modif-grid">
+                Modifier
+              </button>
+              <button type="button" className="refuse-grid">
+                Supprimer
+              </button>
+            </div>
+          </div>
         </div>
       ))}
     </>
