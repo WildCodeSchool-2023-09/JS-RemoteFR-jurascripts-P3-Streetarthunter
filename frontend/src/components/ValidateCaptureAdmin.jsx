c3536284817b05d3fb7a028c6cc0dedf05e30670
Alexandre-27 <alexandre24102000@gmail.com>
2024-02-05 17:14:37 +0100

    Creating the ValidateCaptureAdmin.jsx component and adding tags

---
+++ frontend/src/components/ValidateCaptureAdmin.jsx
@@ -0,0 +1,15 @@
+function ValidateCaptureAdmin() {
+  return (
+    <section>
+      <h2>Valider</h2>
+      <ul>
+        <li>Nouvelle oeuvre</li>
+        <li>Oeuvre difficile à trouver</li>
+        <li>Tous les champs sont renseignés</li>
+        <li>Ajouter cette oeuvre à la galerie générale</li>
+      </ul>
+    </section>
+  );
+}
+
+export default ValidateCaptureAdmin;
