081d67f01e7a70b89f1036dec75d2ea8ecabe691
Alexandre-27 <alexandre24102000@gmail.com>
2024-02-06 12:19:36 +0100

    Calls the setToggleModalCapture props and places it in an onClick of the cross image button which will return its state to false and adds the button to validate the work.

--- frontend/src/components/ValidateCaptureAdmin.jsx
+++ frontend/src/components/ValidateCaptureAdmin.jsx
@@ -1,6 +1,17 @@
-function ValidateCaptureAdmin() {
+import PropTypes from "prop-types";
+import CrossButton from "../assets/picto/yellow/cross_yell.svg";
+
+function ValidateCaptureAdmin({ setToggleModalCapture }) {
   return (
     <section>
+      <button
+        type="button"
+        onClick={() => {
+          setToggleModalCapture(false);
+        }}
+      >
+        <img src={CrossButton} alt="Fermeture du filtre" />
+      </button>
       <h2>Valider</h2>
       <ul>
         <li>
@@ -20,8 +31,13 @@ function ValidateCaptureAdmin() {
           <p>Ajouter cette oeuvre à la galerie générale</p>
         </li>
       </ul>
+      <button type="button">Valider l'oeuvre</button>
     </section>
   );
 }
+
+ValidateCaptureAdmin.propTypes = {
+  setToggleModalCapture: PropTypes.func.isRequired,
+};
 
 export default ValidateCaptureAdmin;
