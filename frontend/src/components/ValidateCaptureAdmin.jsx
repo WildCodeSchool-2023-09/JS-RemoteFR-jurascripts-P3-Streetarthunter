a6dcaec13fe7eac8cb426b3a9fa08b36c3427317
Alexandre-27 <alexandre24102000@gmail.com>
2024-02-06 11:32:35 +0100

    Added input in ValidateCaptureAdmin.jsx component

--- frontend/src/components/ValidateCaptureAdmin.jsx
+++ frontend/src/components/ValidateCaptureAdmin.jsx
@@ -3,10 +3,22 @@ function ValidateCaptureAdmin() {
     <section>
       <h2>Valider</h2>
       <ul>
-        <li>Nouvelle oeuvre</li>
-        <li>Oeuvre difficile à trouver</li>
-        <li>Tous les champs sont renseignés</li>
-        <li>Ajouter cette oeuvre à la galerie générale</li>
+        <li>
+          <input type="checkbox" />
+          <p>Nouvelle oeuvre</p>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <p>Oeuvre difficile à trouver</p>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <p>Tous les champs sont renseignés</p>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <p>Ajouter cette oeuvre à la galerie générale</p>
+        </li>
       </ul>
     </section>
   );
